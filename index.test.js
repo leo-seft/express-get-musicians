c2e11fd4588100e7bf6198a0e01bb7fdaaf7a2a4
leo-seft <leosefton13@hotmail.com>
2024-11-05 16:16:52 +0000

    Adding parts 1&2 and testing

--- index.test.js
+++ index.test.js
@@ -8,15 +8,38 @@ const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
 const {seedMusician} = require("./seedData");
+const exp = require('constants');
+const { deserialize } = require('v8');
 
 
 describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
+    const request = require("supertest");
 
+    test("Testing musicians endpoint", async () => {
+        const response = await request(app).get("/musicians");
+    })
 
+    test("Testing musicians endpoint", async () => {
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200);
+    })
 
+    test("Testing musicians endpoint", async () => {
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(responseData).toBeInstanceOf(Array);
+        expect(responseData.length).toBeGreaterThan(0);
+    })
+})
+describe('/musicians/:id endpoint', () => {
+    test("Testing musicians/:id endpoint with valid id" , async() => {
+        const validId = 1;
+        const response = await request(app).get(`/musicians/${validId}`);
 
-    
+        expect(response.statusCode).toBe(200);
+        
+        expect(response.body).toBeInstanceOf(Object);
+        expect(response.body.id).toBe(validId);
+
+    })
 })
--- src/app.js
+++ src/app.js
@@ -7,8 +7,18 @@ const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 
+app.get("/musicians", async (req, res) => {
+    const musicians = await Musician.findAll()
+    res.json(musicians)
+})
 
+app.get("/musicians/:id", async(req, res) => {
+    const musicianId = req.params.id
+    const musicians = await Musician.findByPk(musicianId)
+    res.status(200).json(musicians)
+})
 
+app.use(express.json())
 
 
 
